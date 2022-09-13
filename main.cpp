1883c020c6602c5a8dde8059202a9c590613b566
Akhilbaran Ghosh <akhilbaranghosh1998@gmail.com>
2022-09-13 20:53:45 +0530

    cyclic rotation of arr[i] = i done.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include "akhil.h"
 using namespace std;
 
+void cyclic_rearrange_arr_i_i(vector<int>& arr){
+    for (int i = 0; i < arr.size();) {
+        if(arr[i] != -1 && arr[i] != arr[arr[i]]){
+            swap(arr[i],arr[arr[i]]);
+        }else{
+            i++;
+        }
+    }
+}
 int main() {
     int n;
     cin >> n;
@@ -8,15 +17,10 @@ int main() {
     for (int i = 0; i < n; ++i) {
         cin >> arr[i];
     }
-    for (int i = 0; i < n;) {
-        if(arr[i] != -1 && arr[i] != arr[arr[i]]){
-            swap(arr[i],arr[arr[i]]);
-        }else{
-            i++;
-        }
-    }
+    cyclic_rearrange_arr_i_i(arr);
     for (int i = 0; i < n; ++i) {
         cout << arr[i] << " ";
     }
     cout << endl;
 }
+// -1 -1 6 1 9 3 2 -1 4 -1
